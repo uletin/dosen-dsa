b2b16c6e5c46e0538c95ec345ebcc88bf86cfa36
Uletin <dokterkoding111@gmail.com>
2024-11-25 20:31:51 +0800

    stack 3

---
+++ array3/index.js
@@ -0,0 +1,15 @@
+const fruits = ["banana", "watermelon", "apple"];
+
+console.log(fruits);
+
+//Menghapus Berdasarkan Index
+// (index, one element)
+fruits.splice(1, 1);
+
+// Menghapus anggota terakhir dari array
+fruits.pop();
+
+//Menghapus anggota pertama dari array
+fruits.shift();
+
+console.log(fruits);
---
+++ stack-3/index.js
@@ -0,0 +1,34 @@
+const readline = require("readline");
+
+const input = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const items = [];
+
+const addItem = (item) => {
+  items.push(item);
+};
+
+const removeItem = () => {
+  items.pop();
+};
+
+const inputQuestion = () => {
+  console.log("Barang di dalam Truck", items);
+
+  input.question(
+    "Pilih Fitur : \n 1. Tambah Barang \n 2. Ambil Barang \n 3. Keluar Aplikasi \n Pilih Nomor Berapa : ",
+    (answer) => {
+      if (answer === "1") {
+        input.question("Masukkan Nama Barang : ", (item) => {
+          addItem(item);
+          inputQuestion();
+        });
+      }
+    }
+  );
+};
+
+inputQuestion();
